a008279037b39c55c13ed88d9d47106da3588a4e
Chris Keefe <crk239@nau.edu>
2021-02-16 20:48:29 -0700

    roughs job scripts for a2-a4

--- a1/a2/jobscript.sh
+++ a1/a2/jobscript.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-
-PREFIX='/scratch/crk239/cs599hpc'
-ASSIGN='a1'
-ACTIVITY='a1'
-INFILE='pingpong_act1_crk239.c'
-OUTFILE='pingpong'
-
-#SBATCH --job-name=ping_pong
-#SBATCH --output=${PREFIX}/${ASSIGN}${ACTIVITY}.out
-#SBATCH --error=${PREFIX}/${ASSIGN}${ACTIVITY}.err
+#SBATCH --account=cs599-spr21
+#SBATCH --job-name=ring2
 #SBATCH --time=02:00				# 2 min
 #SBATCH --mem=2000
 #SBATCH --nodes=1
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 
+PREFIX='/scratch/crk239/cs599hpc'
+ASSIGN='1'
+ACTIVITY='2'
+INFILE='ring_act${ACTIVITY}_crk239.c'
+OUTFILE='ring${ACTIVITY}'
+
 module load openmpi
 
-mpicc ${PREFIX}/${ASSIGN}/${ACTIVITY}/${INFILE} -lm -o ${PREFIX}/${ASSIGN}/${ACTIVITY}/${OUTFILE}
+mpicc ${PREFIX}/a${ASSIGN}/a${ACTIVITY}/${INFILE} -lm -o ${PREFIX}/a${ASSIGN}/a${ACTIVITY}/${OUTFILE}
 
-srun ${PREFIX}/${ASSIGN}/${ACTIVITY}/${OUTFILE}
+srun ${PREFIX}/a${ASSIGN}/a${ACTIVITY}/${OUTFILE}
