f6e68c0a05c2008555f08a7b6bc993f742a8b022
Chris Keefe <crk239@nau.edu>
2021-02-17 19:14:06 -0700

    working a4 random communication

--- a1/a4/random_act4_crk239.c
+++ a1/a4/random_act4_crk239.c
@@ -37,7 +37,59 @@ int main(int argc, char **argv) {
   srand(SEED+my_rank);
 
 
-  //WRITE CODE HERE  
+  //WRITE CODE HERE
+  int root_rank = 0;
+  int source_rank = 0;
+  int dest_rank = 0;
+  int old_counter = 0;
+  int new_counter = 0;
+
+  if (my_rank == root_rank){
+    dest_rank = generateRandomRank(nprocs - 1, my_rank);
+    printf("Master: first rank: %d\n", dest_rank);
+  }
+
+  for (i = 0; i < TOTALITER; i++){
+    MPI_Bcast(&source_rank, 1, MPI_INT, root_rank, MPI_COMM_WORLD);
+    MPI_Bcast(&dest_rank, 1, MPI_INT, root_rank, MPI_COMM_WORLD);
+    // printf("my_rank = %d, dest_rank = %d\n", my_rank, dest_rank);
+
+    if(my_rank == dest_rank){
+      MPI_Recv(&old_counter, 1, MPI_INT, source_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("My rank: %d, old counter: %d\n", my_rank, old_counter);
+
+      new_counter = old_counter + my_rank;
+      printf("My rank: %d, new counter: %d\n", my_rank, new_counter);
+
+      // - calculate next to receive rank
+      dest_rank = generateRandomRank(nprocs - 1, my_rank);
+      printf("My rank: %d, next to recv: %d\n", my_rank, dest_rank);
+
+      // send new dest rank to root so it can prepare for broadcast
+      MPI_Send(&dest_rank, 1, MPI_INT, root_rank, 0, MPI_COMM_WORLD);
+
+
+    } else if (my_rank == 0){
+      // Handle the initial case where root is also the source rank
+      if (my_rank == source_rank){
+        MPI_Send(&new_counter, 1, MPI_INT, dest_rank, 0, MPI_COMM_WORLD);
+      }
+      // currently-active rank becomes source rank for next round
+      source_rank = dest_rank;
+      // receive new dest_rank from currently active rank
+      MPI_Recv(&dest_rank, 1, MPI_INT, dest_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      // source and dest are updated and ready to broadcast
+
+
+    } else if (my_rank == source_rank) {
+      // NOTE: this doesn't get called on the first send when source is root
+      MPI_Send(&new_counter, 1, MPI_INT, dest_rank, 0, MPI_COMM_WORLD);
+
+
+    } else {
+      // ranks that aren't source or dest idle 
+    }
+  }
 
   
 
