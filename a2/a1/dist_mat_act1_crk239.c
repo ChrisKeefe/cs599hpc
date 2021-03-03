132e2ba7fbcd8829ac8fe8c02c28682814dd6baf
Chris Keefe <crk239@nau.edu>
2021-03-03 11:14:17 -0700

    Fixes janky formatting

--- a2/a1/dist_mat_act1_crk239.c
+++ a2/a1/dist_mat_act1_crk239.c
@@ -5,8 +5,6 @@
 #include <math.h>
 #include <string.h>
 
-
-
 //Example compilation
 //mpicc distance_matrix_starter.c -lm -o distance_matrix_starter
 
@@ -14,19 +12,17 @@
 //mpirun -np 1 -hostfile myhostfile.txt ./distance_matrix_starter 100000 90 100 MSD_year_prediction_normalize_0_1_100k.txt
 //mpirun -np 1 -hostfile ../myhostfile.txt ./dm 100 90 100 ../MSD_year_prediction_normalize_0_1_100k.txt
 
-
 //function prototypes
-int importDataset(char * fname, int N, double ** dataset);
+int importDataset(char *fname, int N, double **dataset);
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
   int my_rank, nprocs;
 
-  MPI_Init(&argc,&argv);
-  MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-  MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
   //Process command-line arguments
   int N;
@@ -34,23 +30,22 @@ int main(int argc, char **argv) {
   int blocksize;
   char inputFname[500];
 
-
-  if (argc != 5) {
-    fprintf(stderr,"Please provide the following on the command line: N (number of lines in the file), dimensionality (number of coordinates per point), block size, dataset filename.\n");
+  if (argc != 5)
+  {
+    fprintf(stderr, "Please provide the following on the command line: N (number of lines in the file), dimensionality (number of coordinates per point), block size, dataset filename.\n");
     MPI_Finalize();
     exit(0);
   }
 
-  sscanf(argv[1],"%d",&N);
-  sscanf(argv[2],"%d",&DIM);
-  sscanf(argv[3],"%d",&blocksize);
-  strcpy(inputFname,argv[4]);
-  
-  //pointer to dataset
-  double ** dataset;
+  sscanf(argv[1], "%d", &N);
+  sscanf(argv[2], "%d", &DIM);
+  sscanf(argv[3], "%d", &blocksize);
+  strcpy(inputFname, argv[4]);
 
-  
-  if (N<1 || DIM <1)
+  //pointer to dataset
+  double **dataset;
+
+  if (N < 1 || DIM < 1)
   {
     printf("\nN is invalid or DIM is invalid\n");
     MPI_Finalize();
@@ -59,151 +54,151 @@ int main(int argc, char **argv) {
   else
   //All ranks import dataset
   {
-   
-    if (my_rank==0)
+
+    if (my_rank == 0)
     {
-    printf("\nNumber of lines (N): %d, Dimensionality: %d, Block size: %d, Filename: %s\n", N, DIM, blocksize, inputFname);
+      printf("\nNumber of lines (N): %d, Dimensionality: %d, Block size: %d, Filename: %s\n", N, DIM, blocksize, inputFname);
     }
 
     //allocate memory for dataset
-    dataset=(double**)malloc(sizeof(double*)*N);
-    for (int i=0; i<N; i++)
+    dataset = (double **)malloc(sizeof(double *) * N);
+    for (int i = 0; i < N; i++)
     {
-      dataset[i]=(double*)malloc(sizeof(double)*DIM);
+      dataset[i] = (double *)malloc(sizeof(double) * DIM);
     }
 
     // import data set...
-    int ret=importDataset(inputFname, N, dataset);
+    int ret = importDataset(inputFname, N, dataset);
 
     // ... failing fast if import fails.
-    if (ret==1)
+    if (ret == 1)
     {
       MPI_Finalize();
       return 0;
     }
-
   }
 
   //Write code here
-	// at the start, every rank has a local copy of the data.
+  // at the start, every rank has a local copy of the data.
 
-	// Each rank is responsible for computing all distances for N/nprocs rows (points)
-	// If N isn't a multiple of nprocs, assign extra rows only to the last rank.
-	unsigned int n_rows_per_rank = N / nprocs;
-	unsigned int n_rows_last_rank = n_rows_per_rank + N % nprocs;
-	unsigned int loc_start_idx = 0;
-	unsigned int start_indices[nprocs];
-	unsigned int dm_idx = 0;
+  // Each rank is responsible for computing all distances for N/nprocs rows (points)
+  // If N isn't a multiple of nprocs, assign extra rows only to the last rank.
+  unsigned int n_rows_per_rank = N / nprocs;
+  unsigned int n_rows_last_rank = n_rows_per_rank + N % nprocs;
+  unsigned int loc_start_idx = 0;
+  unsigned int start_indices[nprocs];
+  unsigned int dm_idx = 0;
 
-printf("R: %d, N: %d, n_rows_per_rank: %d\n", my_rank, N, n_rows_per_rank);
+  printf("R: %d, N: %d, n_rows_per_rank: %d\n", my_rank, N, n_rows_per_rank);
 
-	// Populate array of start indices for Scatter
-	if (my_rank == 0){
-		// printf("%d rows, %d ranks, %d rows per rank, except %d rows in the last rank\n",
-		//        N, nprocs, n_rows_per_rank, n_rows_last_rank );
-		for (int i = 0; i < nprocs; i++){
-			start_indices[i] = i * n_rows_per_rank;
-		}
-	}
+  // Populate array of start indices for Scatter
+  if (my_rank == 0)
+  {
+    // printf("%d rows, %d ranks, %d rows per rank, except %d rows in the last rank\n",
+    //        N, nprocs, n_rows_per_rank, n_rows_last_rank );
+    for (int i = 0; i < nprocs; i++)
+    {
+      start_indices[i] = i * n_rows_per_rank;
+    }
+  }
 
-	// Send start indices to all ranks
-	MPI_Scatter(&start_indices, 1, MPI_UNSIGNED, &loc_start_idx, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-	// printf("Rank: %d, Start index: %d\n", my_rank, loc_start_idx);
+  // Send start indices to all ranks
+  MPI_Scatter(&start_indices, 1, MPI_UNSIGNED, &loc_start_idx, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  // printf("Rank: %d, Start index: %d\n", my_rank, loc_start_idx);
 
-	// allocate memory on all ranks for an n_rows_per_rank x N cols subset of the dm
-	if (my_rank == nprocs - 1){
-		n_rows_per_rank = n_rows_last_rank;
-	}
-	double *local_dm_block = (double *) calloc(n_rows_per_rank * N, sizeof(double));
+  // allocate memory on all ranks for an n_rows_per_rank x N cols subset of the dm
+  if (my_rank == nprocs - 1)
+  {
+    n_rows_per_rank = n_rows_last_rank;
+  }
+  double *local_dm_block = (double *)calloc(n_rows_per_rank * N, sizeof(double));
 
-printf("R: %d, n_rows_per_rank: %d\n", my_rank, n_rows_per_rank);
-printf("R: %d, Number of slots in local_dm_block: %d\n", my_rank, n_rows_per_rank * N);
+  printf("R: %d, n_rows_per_rank: %d\n", my_rank, n_rows_per_rank);
+  printf("R: %d, Number of slots in local_dm_block: %d\n", my_rank, n_rows_per_rank * N);
 
   double time;
-  if (my_rank == 0){
-	  time = MPI_Wtime();
+  if (my_rank == 0)
+  {
+    time = MPI_Wtime();
   }
 
-	int loc_stop_idx = loc_start_idx + n_rows_per_rank;
-	for (int pt_a = loc_start_idx; pt_a < loc_stop_idx; pt_a++){
-		// To drop out lower triangle, try moving pt_b = 0 to pt_b = pt_a
-		for (int pt_b = 0; pt_b < N; pt_b++){
-			// TODO: Calculate distance
+  int loc_stop_idx = loc_start_idx + n_rows_per_rank;
+  for (int pt_a = loc_start_idx; pt_a < loc_stop_idx; pt_a++)
+  {
+    // To drop out lower triangle, try moving pt_b = 0 to pt_b = pt_a
+    for (int pt_b = 0; pt_b < N; pt_b++)
+    {
+      // TODO: Calculate distance
       double dist = 5.0;
-			dm_idx = (pt_a - loc_start_idx) * N + pt_b;
-			local_dm_block[dm_idx] = dist;
-		}
-	}
-	printf("value at 0: %lf, value at 1: %lf\n", local_dm_block[0], local_dm_block[1]);
+      dm_idx = (pt_a - loc_start_idx) * N + pt_b;
+      local_dm_block[dm_idx] = dist;
+    }
+  }
+  printf("value at 0: %lf, value at 1: %lf\n", local_dm_block[0], local_dm_block[1]);
 
-	// Calculate local sums at each rank
-  if (my_rank == 0){
-	  time = MPI_Wtime() - time;
+  // Calculate local sums at each rank
+  if (my_rank == 0)
+  {
+    time = MPI_Wtime() - time;
   }
   // TODO: display time
-	// output the distance matrix (sequentially)
-	// Have rank 0 MPI_Reduce the local sums into a single global sum. 
-	// Report this in the writeup.
+  // output the distance matrix (sequentially)
+  // Have rank 0 MPI_Reduce the local sums into a single global sum.
+  // Report this in the writeup.
 
+  free(local_dm_block);
+  //free dataset
+  for (int i = 0; i < N; i++)
+  {
+    free(dataset[i]);
+  }
 
+  free(dataset);
 
-	free(local_dm_block);
-	//free dataset
-	for (int i=0; i<N; i++)
-	{
-		free(dataset[i]);
-	}
+  MPI_Finalize();
 
-	free(dataset);
-
-	MPI_Finalize();
-
-	return 0;
+  return 0;
 }
 
-
-
-
-int importDataset(char * fname, int N, double ** dataset)
+int importDataset(char *fname, int N, double **dataset)
 {
 
-		FILE *fp = fopen(fname, "r");
+  FILE *fp = fopen(fname, "r");
 
-		if (!fp) {
-				printf("Unable to open file\n");
-				return(1);
-		}
+  if (!fp)
+  {
+    printf("Unable to open file\n");
+    return (1);
+  }
 
-		char buf[4096];
-		int rowCnt = 0;
-		int colCnt = 0;
-		while (fgets(buf, 4096, fp) && rowCnt<N) {
-				colCnt = 0;
+  char buf[4096];
+  int rowCnt = 0;
+  int colCnt = 0;
+  while (fgets(buf, 4096, fp) && rowCnt < N)
+  {
+    colCnt = 0;
 
-				char *field = strtok(buf, ",");
-				double tmp;
-				sscanf(field,"%lf",&tmp);
-				dataset[rowCnt][colCnt]=tmp;
+    char *field = strtok(buf, ",");
+    double tmp;
+    sscanf(field, "%lf", &tmp);
+    dataset[rowCnt][colCnt] = tmp;
 
-				
-				while (field) {
-					colCnt++;
-					field = strtok(NULL, ",");
-					
-					if (field!=NULL)
-					{
-					double tmp;
-					sscanf(field,"%lf",&tmp);
-					dataset[rowCnt][colCnt]=tmp;
-					}   
+    while (field)
+    {
+      colCnt++;
+      field = strtok(NULL, ",");
 
-				}
-				rowCnt++;
-		}
+      if (field != NULL)
+      {
+        double tmp;
+        sscanf(field, "%lf", &tmp);
+        dataset[rowCnt][colCnt] = tmp;
+      }
+    }
+    rowCnt++;
+  }
 
-		fclose(fp);
+  fclose(fp);
 
-		return 0;
+  return 0;
 }
-
-
