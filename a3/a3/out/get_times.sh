3d79db44bcc722266d5cfb166b087cbe3a9a72cb
ChrisKeefe <crk239@nau.edu>
2021-03-17 09:16:49 -0700

    Results act 3

--- a3/a3/out/get_times.sh
+++ a3/a3/out/get_times.sh
@@ -3,13 +3,13 @@
 for NPROCS in 1 2 4 8 12 16 20; do
     echo sort${NPROCS} >> timings.txt
 	for ITER in 1 2 3 ; do
-	sed -n '1p' ./sort${NPROCS}_${ITER}.out >> timings.out
+	sed -n '2p' ./sort${NPROCS}_${ITER}.out >> timings.txt
     done
 done
 
 for NPROCS in 1 2 4 8 12 16 20; do
     echo sort${NPROCS} >> global_sums.txt
 	for ITER in 1 2 3 ; do
-	sed -n '3p' ./sort${NPROCS}_${ITER}.out >> global_sums.out
+	sed -n '4p' ./sort${NPROCS}_${ITER}.out >> global_sums.txt
     done
 done
