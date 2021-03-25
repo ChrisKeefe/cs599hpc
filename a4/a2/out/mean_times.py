9baa3a04e9245320c3b8bb5e0a8351ec588a770f
Chris Keefe <crk239@nau.edu>
2021-03-24 17:36:49 -0700

    updates summary script, adds summary data

--- a4/a2/out/mean_times.py
+++ a4/a2/out/mean_times.py
@@ -1,7 +1,7 @@
 from statistics import mean
-num_iterations = 2
+num_iterations = 3
 num_processor_counts = 6
-error_offset = 2
+error_offset = 0
 write_lines = []
 sums = []
 
@@ -12,26 +12,53 @@ with open("./timings.txt", "r") as fp:
     for line in lines:
         flt = 0.0
         try:
-            flt = float(line.split()[-1])
-            # print(flt)
-            vals.append(flt)
+            spl = line.split()
+            for word in range(len(spl)):
+                spl[word] = spl[word].rstrip(',')
+            build = float(spl[3])
+            query = float(spl[5])
+            el = float(spl[-1])
+            vals.append((build, query, el))
         except ValueError:
             pass
+        except IndexError:
+            pass
 
-    for group in range(0, len(vals), num_iterations):
+    for i, group_st in enumerate(range(0, len(vals), num_iterations)):
+        group_ctr = 0
+        if group_st == 0:
+            nprocs = 1
+        else:
+            nprocs = 4 * i
+        group_end = group_st + num_iterations
+        grouples = vals[group_st: group_end]
+        build_mean = 0
+        que_mean = 0
+        tot_mean = 0
+        for i in range(num_iterations):
+            build_mean += grouples[i][0]
+            que_mean += grouples[i][1]
+            tot_mean += grouples[i][2]
+        build_mean /= num_iterations
+        que_mean /= num_iterations
+        tot_mean /= num_iterations
         write_lines.append(
-            f"NPROCS = {group * 2}, "
-            f"mean = {round(mean(vals[group:group + num_iterations]), 4)}\n")
+            f"NPROCS = {nprocs}, "
+            f"build = {round(build_mean, 4)}, "
+            f"query = {round(que_mean, 4)}, "
+            f"total = {round(tot_mean, 4)}, ")
+        print(write_lines)
 
 # Read in global sums
 with open("./global_sums.txt", "r") as sum_fp:
     lines = sum_fp.readlines()
+    sums = []
     for group in range(num_processor_counts):
-        val = 0
         try:
-            sums.append(int(lines[group * (num_iterations + 2) + error_offset].split()[-1]))
+            sums.append(int(lines[group * (num_iterations + 1) + 1].split()[-1]))
         except ValueError as err:
             pass
+    print(sums)
 
 # Write to a summary document
 with open("./summary.txt", "w") as w_fp:
