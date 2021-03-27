94132d2d6510ec170184e823135d3d50810881f7
ChrisKeefe <crk239@nau.edu>
2021-03-27 15:10:05 -0700

    jobscript cleanup

--- a4/a3e/jobscript.sh
+++ a4/a3e/jobscript.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=cs599-spr21
 #SBATCH --job-name=range3e          # In practice, this is superseded from the CLI
 #SBATCH --time=1:00 	            # 1 min
-#SBATCH --mem=0                     # We are exclusive: 0 -> use all available mem
+#SBATCH --mem=16000                 
 #SBATCH --cpus-per-task=1
 #SBATCH -C sl                       # there are a bunch of broadwell (bw) and skylake (sl) cores
 
