4d300e99317d386bd8d4dc4bebd0da5127b738f7
Chris Keefe <crk239@nau.edu>
2021-04-12 11:06:16 -0700

    updates get_times script

--- a5/a1/out/get_times.sh
+++ a5/a1/out/get_times.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 for NPROCS in 4 8 12 16 20; do
-    echo range${NPROCS} >> timings.txt
-	for ITER in 1 2 3 ; do
-	sed -n '2p' ./range${NPROCS}_${ITER}.out >> timings.txt
-    # use awk NR and NF and exec to average the timings
-    done
-done
-
-for NPROCS in 4 8 12 16 20; do
-    echo range${NPROCS} >> global_sums.txt
-	for ITER in 1 2 3 ; do
-	sed -n '1p' ./range${NPROCS}_${ITER}.out >> global_sums.txt
+    for K in 2 25 50 100; do
+      echo kmeans${NPROCS}_${K} >> timings.txt
+	  for ITER in 1 2 3 ; do
+	    tail -n 1 ./kmeans${NPROCS}_${K}_${ITER}.out >> timings.txt
+      done
     done
 done
