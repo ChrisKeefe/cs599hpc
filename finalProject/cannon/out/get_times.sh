34b2a25e6d3a538795f2c209359bd3d2d97d47f4
Chris Keefe <crk239@nau.edu>
2021-04-14 22:01:07 -0700

    Adds data summary scripts

---
+++ finalProject/cannon/out/get_times.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for NPROCS in 1; do
+      echo cannon${NPROCS} >> timings.txt
+	  for ITER in 0 1 2 3 4 5 6 7 8 9; do
+	    tail -n 1 ./canon${NPROCS}_${ITER}.out >> timings.txt
+      done
+    done
+done
