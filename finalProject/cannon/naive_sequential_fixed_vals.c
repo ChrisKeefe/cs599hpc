421a35030a6872487d3b3d567a19de70e4188669
Chris Keefe <crk239@nau.edu>
2021-04-14 19:56:15 -0700

    Modularity and safety from int overflows

--- finalProject/cannon/naive_sequential_fixed_vals.c
+++ finalProject/cannon/naive_sequential_fixed_vals.c
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 3
-
-// Allocate Arrays
-int arrA[N][N];
-int arrB[N][N];
-int arrC[N][N];
+#define N 4
+void naive_multiply(int **arrA, int **arrB, int **arrC);
+void print_mtrx_shared_int(int ** matrix, char *name);
 
 int main(int argc, char *argv) {
   int i, j, k;
+  // Allocate Arrays
+  int **arrA = (int **)malloc(sizeof(int *) * N);
+  int **arrB = (int **)malloc(sizeof(int *) * N);
+  int **arrC = (int **)malloc(sizeof(int *) * N);
+
+  for (int i=0; i<N; i++)
+  {
+    arrA[i]=(int*)malloc(sizeof(int) * N);
+    arrB[i]=(int*)malloc(sizeof(int) * N);
+    arrC[i]=(int*)malloc(sizeof(int) * N);
+  }
 
   // populate arrays
-  int a[9] = {1, 5, 3, 4, 2, 2, 3, 2, 5};
-  int b[9] = {2, 1, 1, 1, 2, 1, 2, 1, 2};
-
+  int a[16] = {1, 5, 3, 4, 2, 2, 3, 2, 5, 3, 3, 3, 1, 2, 3, 5};
+  int b[16] = {2, 1, 1, 1, 2, 1, 2, 1, 2, 3, 3, 3, 1, 2, 2, 1};
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
       arrA[i][j] = a[i*N + j];
@@ -24,22 +31,32 @@ int main(int argc, char *argv) {
     }
   }
 
-  
-  // perform naive matrix multiplication
-  for (i = 0; i < N; i++)
-    for (k = 0; k < N; k++)
-      for (j = 0; j < N; j++) 
-        arrC[i][j] += arrA[i][k] * arrB[k][j];
+  print_mtrx_shared_int(arrA, "A");
+  print_mtrx_shared_int(arrB, "B");
 
-  // print resulting matrix
-  // We can do this blockwise for MPI impl. See a2/act1
-  // TODO: this should be CL flag toggleable
+  naive_multiply(arrA, arrB, arrC);
+
+  print_mtrx_shared_int(arrC, "C");
+
+  exit(0);
+}
+
+// perform naive matrix multiplication
+void naive_multiply(int **arrA, int **arrB, int **arrC){
+  for (int i = 0; i < N; i++)
+    for (int k = 0; k < N; k++)
+      for (int j = 0; j < N; j++)
+        arrC[i][j] += arrA[i][k] * arrB[k][j];
+}
+
+void print_mtrx_shared_int(int ** matrix, char *name){
+  int i, k;
+  printf("Matrix %s\n", name);
   for (i = 0; i < N; i++){
     for (k = 0; k < N; k++){
-      printf("%-12d ", arrC[i][k]);
+      printf("%-12d ", matrix[i][k]);
     }
     printf("\n");
   }
-
-  exit(0);
+  printf("\n");
 }
---
+++ finalProject/notes.md
@@ -0,0 +1,5 @@
+## Project notes for writeup
+
+## 4/14
+- modularized sequential code
+- Updated int sizes and manually capped RAND_MAX to guarantee no integer overflows. Code now uses unsigned long long ints for C matrix and ints for A/B
